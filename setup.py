ab87b759055aea13f7f922e3488f8b11c786f7ee
aa10sh <asur7326@gmail.com>
2026-01-27 16:34:45 +0530

    setup ready for data ingestion in MongoDB

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Adarsh Singh",
+    author_email="asur7326@gmail.com",
+    packages=find_packages()
+)
